2caacb6cc4c8d78358c0bbb4b1f63c373a710515
cclark012 <109385164+cclark012@users.noreply.github.com>
2026-05-31 16:31:32 -0600

    feat: Add a batch script to start the GUI rather than the user having to go through the command line.

---
+++ start-gui.bat
@@ -0,0 +1,33 @@
+:: Name:    start-gui.bat
+:: Purpose: Starts the spectroscopy GUI with WinPython
+
+@echo off
+
+title Spectroscopy GUI
+color 0B
+echo %date%
+:: echo %time%
+
+setlocal enableextensions
+set me=%~n0
+set parent=%~dp0
+
+:: echo Starting the GUI...
+
+set "target_file=%~dp0..\WinPython\python\python.exe"
+
+:: Provide a Y/N choice, defaulting to Y after 5 seconds
+choice /c YN /N /T 5 /D Y /M "Emulate spectrometer, power meter, and lasers? [Y/N]"
+goto:sub_%ERRORLEVEL%
+
+:sub_1
+echo Starting the GUI with emulation...
+%target_file% %~dp0gui.py --emulate --laser-mode emulated
+goto:eof
+
+:sub_2
+echo Starting the GUI with real instruments...
+%target_file% %~dp0gui.py --real --obis-ports COM3 COM5
+goto:eof
+
+:: echo %target_file%
